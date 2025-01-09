60f49bb16fc250d1b69afef59f8ad6e9f0d3907e
Anil Pal <anilcodes01@gmail.com>
2025-01-09 09:34:52 +0530

    added middleware to protect routes and pages

---
+++ middleware.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const sessionToken = req.cookies.get("next-auth.session-token")?.value;
+
+  const { pathname } = req.nextUrl;
+
+  if (!sessionToken && pathname === "/info") {
+    return NextResponse.next();
+  }
+
+  if (!sessionToken) {
+    return NextResponse.redirect(new URL("/info", req.url));
+  }
+
+  if (sessionToken && pathname === "/info") {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/",
+    "/info",
+    "/user/:path*",
+    "/articles/:path*",
+    "/bookmarks/:path*",
+    "/network/:path*",
+    "/post/:path*",
+    "/search/:path*",
+    "/polls/:path*",
+  ],
+};
