a10193368d6cc3667892172b7a45350d90cd6e67
Anil Pal <anilcodes01@gmail.com>
2025-01-10 12:48:03 +0530

    added username to authoptions

--- app/lib/authOptions.ts
+++ app/lib/authOptions.ts
@@ -5,9 +5,8 @@ import bcrypt from "bcryptjs";
 import { JWT } from "next-auth/jwt";
 import { Session, User } from "next-auth";
 
-
 interface CustomUser extends User {
-  avatarUrl?: string; 
+  avatarUrl?: string;
 }
 
 export const authOptions = {
@@ -15,8 +14,16 @@ export const authOptions = {
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        email: { label: "Email", type: "text", placeholder: "Enter your Email" },
-        password: { label: "Password", type: "password", placeholder: "Enter your password" },
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "Enter your Email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter your password",
+        },
       },
       async authorize(credentials) {
         if (!credentials) return null;
@@ -29,10 +36,9 @@ export const authOptions = {
           return null;
         }
 
-       
         if (!user.password) {
           console.log("User password is missing");
-          return null; 
+          return null;
         }
 
         const isValidPassword = await bcrypt.compare(password, user.password);
@@ -46,26 +52,24 @@ export const authOptions = {
           name: user.name,
           email: user.email,
           avatarUrl: user.avatarUrl ?? undefined,
-        } as CustomUser; 
+        } as CustomUser;
       },
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
       async profile(profile) {
-       
         let user = await prisma.user.findUnique({
           where: { email: profile.email },
         });
 
-        
         if (!user) {
           user = await prisma.user.create({
             data: {
               name: profile.name,
+              username: profile.username,
               email: profile.email,
-              avatarUrl: profile.picture, 
-              
+              avatarUrl: profile.picture,
             },
           });
         }
@@ -89,7 +93,7 @@ export const authOptions = {
       }
       return token;
     },
-    session: ({ session, token }: { session: Session; token: JWT }) => { 
+    session: ({ session, token }: { session: Session; token: JWT }) => {
       if (session && session.user) {
         session.user.id = token.id as string;
         session.user.name = token.name;
