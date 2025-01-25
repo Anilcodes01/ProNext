1e2d3ea4fc0130f34ca0e1682af5634e6ac7aa28
Anil Pal <anilcodes01@gmail.com>
2025-01-25 13:58:00 +0530

    fixed deployment error

--- components/Error.tsx
+++ components/Error.tsx
@@ -1,23 +1,23 @@
-import { ErrorProps } from "@/types/types";
-import {  Smile } from "lucide-react";
+// import { ErrorProps } from "@/types/types";
+// import {  Smile } from "lucide-react";
 
-export function Error({ type }: ErrorProps) {
-  if (type === "no-session") {
-    return (
-      <div className="flex items-center justify-center h-full">
-        <div className="text-center">
-          <Smile className="w-12 h-12 text-yellow-500 mb-4" />
-          <h2 className="text-2xl font-semibold text-gray-900">
-            Oops! It looks like you're not signed in.
-          </h2>
-        </div>
-      </div>
-    );
-  }
+// export function Error({ type }: ErrorProps) {
+//   if (type === "no-session") {
+//     return (
+//       <div className="flex items-center justify-center h-full">
+//         <div className="text-center">
+//           <Smile className="w-12 h-12 text-yellow-500 mb-4" />
+//           <h2 className="text-2xl font-semibold text-gray-900">
+//             Oops! It looks like you're not signed in.
+//           </h2>
+//         </div>
+//       </div>
+//     );
+//   }
 
-  if(type === 'ai-error') {
-    return <div>
-        <p className="text-gray-800">AI don't want to enhance your post...!</p>
-    </div>
-  }
-}
+//   if(type === 'ai-error') {
+//     return <div>
+//         <p className="text-gray-800">AI don't want to enhance your post...!</p>
+//     </div>
+//   }
+// }
