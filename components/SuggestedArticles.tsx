ef3b501694442fb17ccdba3639bdcb60f85d8ca9
Anil Pal <anilcodes01@gmail.com>
2025-02-05 15:30:16 +0530

    fixed vercel errors

--- components/SuggestedArticles.tsx
+++ components/SuggestedArticles.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { MoveRight } from "lucide-react";
 import { useRouter } from "next/navigation";
 import SuggesstionbarSkeleton from "./skeletons/SuggestionbarSkeleton";
