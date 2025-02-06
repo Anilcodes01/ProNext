9b9edbb2a6412fb44750a0981d44d3709207e00d
Anil Pal <anilcodes01@gmail.com>
2025-02-06 13:38:56 +0530

    added context to suggestedUsers

--- components/SuggestedUsers.tsx
+++ components/SuggestedUsers.tsx
@@ -1,37 +1,23 @@
 "use client";
-
-import axios from "axios";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import SuggestedUsersSkeleton from "./skeletons/SuggestedUsersSkeleton";
 import { useRouter } from "next/navigation";
+import { ExtendedUser } from "@/types/types";
+import { useUsers } from "@/context/UsersContext";
 
-interface User {
-  id: string;
-  name: string;
-  username: string;
-  avatarUrl?: string;
-}
+const getRandomUsers = (users: ExtendedUser[], count: number) => {
+  const shuffled = [...users].sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, count);
+};
 
 export default function SuggestedUsers() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, error, fetchUsers } = useUsers();
   const router = useRouter();
 
   useEffect(() => {
-    const suggestedUsers = async () => {
-      try {
-        const response = await axios.get("/api/users/suggestedUsers");
-        const usersData = response.data.users;
-        setUsers(usersData);
-      } catch (error) {
-        console.error("Error fetching suggested users", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    suggestedUsers();
-  }, []);
+    fetchUsers();
+  }, [fetchUsers]);
 
   if (loading) {
     return (
@@ -41,24 +27,32 @@ export default function SuggestedUsers() {
     );
   }
 
+  if (error) {
+    return (
+      <div className="text-center text-red-600">
+        Error loading suggested users
+      </div>
+    );
+  }
+
+  const suggestedUsers = getRandomUsers(users, 2);
   return (
     <div className="border border-gray-100 text-black gap-4 rounded-lg flex flex-col p-4">
       <span>Who to follow</span>
       <div className="flex flex-col gap-4 ">
-        {users.length > 0 ? (
-          users.map((user) => (
+        {suggestedUsers.map((user) => (
+          <div
+            key={user.id}
+            className="flex gap-2 justify-between items-center"
+          >
             <div
-              key={user.id}
-              className="flex gap-2 justify-between items-center"
+              onClick={() => {
+                router.push(`/user/${user.id}`);
+              }}
+              className="flex cursor-pointer"
             >
-              <div
-                onClick={() => {
-                  router.push(`/user/${user.id}`);
-                }}
-                className="flex cursor-pointer"
-              >
-                {user.avatarUrl ? (
-                  <Image
+              {user.avatarUrl ? (
+                <Image
                   src={user.avatarUrl || ""}
                   alt="User Profile"
                   width={250}
@@ -66,29 +60,26 @@ export default function SuggestedUsers() {
                   quality={75}
                   className="rounded-full overflow-hidden h-10 w-10 object-cover cursor-pointer"
                 />
-                ): (
-                  <div>
-                    <div className="flex items-center justify-center cursor-pointer h-10 w-10 rounded-full border bg-green-600 text-white">
-                      {user.name?.charAt(0).toUpperCase()}
-                    </div>
+              ) : (
+                <div>
+                  <div className="flex items-center justify-center cursor-pointer h-10 w-10 rounded-full border bg-green-600 text-white">
+                    {user.name?.charAt(0).toUpperCase()}
                   </div>
-                )}
-                <div className="flex ml-3 flex-col">
-                  <span className="text-base ml-1">{user.name}</span>
-                  <span className="text-[12px] text-gray-400">
-                    @{user.username}
-                  </span>
                 </div>
+              )}
+              <div className="flex ml-3 flex-col">
+                <span className="text-base ml-1">{user.name}</span>
+                <span className="text-[12px] text-gray-400">
+                  @{user.username}
+                </span>
               </div>
-
-              <button className="border rounded-full px-2 py-1 text-[14px] hover:bg-sky-50 border-green-500 text-green-600">
-                Follow
-              </button>
             </div>
-          ))
-        ) : (
-          <div>No users found...!</div>
-        )}
+
+            <button className="border rounded-full px-2 py-1 text-[14px] hover:bg-sky-50 border-green-500 text-green-600">
+              Follow
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
--- components/userCard.tsx
+++ components/userCard.tsx
@@ -1,60 +1,30 @@
 "use client";
-
-import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { FaUserCircle } from "react-icons/fa";
-import FollowButton from "./follow";
 import { FaArrowLeft } from "react-icons/fa6";
-import { IoLocationOutline } from "react-icons/io5";
-
-interface User {
-  id: string;
-  name: string | null;
-  avatarUrl: string | null;
-  bio: string;
-  website: string;
-  city: string;
-}
+import { useUsers } from "@/context/UsersContext";
+import { useEffect } from "react";
 
 interface Follow {
   followingId: string;
 }
 
 export default function UserCard() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [following, setFollowing] = useState<string[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [hoveredUser, setHoveredUser] = useState<User | null>(null);
-  const [hoveredUserPosition, setHoveredUserPosition] = useState<{
-    top: number;
-    left: number;
-  } | null>(null);
+  const { users, loading, error, fetchUsers } =
+    useUsers();
   const router = useRouter();
 
   useEffect(() => {
-    async function fetchUsersAndFollowing() {
-      try {
-        const [usersResponse, followingResponse] = await Promise.all([
-          axios.get("/api/users"),
-          axios.get("/api/follow"),
-        ]);
-        setUsers(usersResponse.data.users);
-        console.log(usersResponse.data);
-        setFollowing(
-          followingResponse.data.following.map(
-            (follow: Follow) => follow.followingId
-          )
-        );
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchUsersAndFollowing();
-  }, []);
+    fetchUsers();
+  }, [fetchUsers]);
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center text-red-600 font-bold text-xl">
+        <p>Error while fetching users...!</p>
+      </div>
+    );
+  }
 
   const Skeleton = () => (
     <div className="border bg-gray-200 p-2 w-auto rounded-xl flex items-center gap-2 animate-pulse">
@@ -66,23 +36,11 @@ export default function UserCard() {
     </div>
   );
 
-  const handleMouseEnter = (event: React.MouseEvent, user: User) => {
-    const { top, left } = event.currentTarget.getBoundingClientRect();
-    setHoveredUserPosition({ top, left });
-    setHoveredUser(user);
-  };
-
-  const handleMouseLeave = () => {
-    setHoveredUser(null);
-    setHoveredUserPosition(null);
-  };
-
   return (
     <div className="bg-white text-black p-4 relative">
-      {/* Mobile Back Button */}
       <div
         onClick={() => router.push("/")}
-        className="flex gap-4 cursor-pointer mb-4 items-center hidden md:flex"
+        className=" gap-4 cursor-pointer mb-4 items-center hidden md:flex"
       >
         <div className="h-8 w-8 hover:bg-gray-200 flex items-center justify-center rounded-full">
           <FaArrowLeft size={20} className="text-black" />
@@ -109,11 +67,7 @@ export default function UserCard() {
             >
               <div className="cursor-pointer w-full bg-white p-2 flex items-center gap-2">
                 {user.avatarUrl ? (
-                  <div
-                    onMouseEnter={(event) => handleMouseEnter(event, user)}
-                    onMouseLeave={handleMouseLeave}
-                    className="w-8 h-8 rounded-full overflow-hidden"
-                  >
+                  <div className="w-8 h-8 rounded-full overflow-hidden">
                     <Image
                       src={user.avatarUrl}
                       alt={user.name || "null"}
@@ -131,11 +85,6 @@ export default function UserCard() {
                   {user.name || "Unnamed User"}
                 </div>
               </div>
-
-              <FollowButton
-                isFollowing={following.includes(user.id)}
-                followingId={user.id}
-              />
             </div>
           ))
         ) : (
@@ -146,60 +95,6 @@ export default function UserCard() {
           </>
         )}
       </div>
-
-      {hoveredUser && hoveredUserPosition && (
-        <div
-          className="absolute z-10 max-w-96 min-w-64 bg-white border rounded-lg shadow-lg p-4 transition-transform duration-300 ease-in-out"
-          style={{
-            top: `${hoveredUserPosition.top + window.scrollY}px`,
-            left: `${hoveredUserPosition.left + window.scrollX}px`,
-            transform: "translate(-50%, -100%)",
-            opacity: 1,
-          }}
-        >
-          <div className="flex flex-col items-start">
-            {hoveredUser.avatarUrl ? (
-              <Image
-                src={hoveredUser.avatarUrl}
-                alt={hoveredUser.name || "User Avatar"}
-                width={192}
-                height={192}
-                className="rounded-full h-20 w-20 object-cover"
-              />
-            ) : (
-              <FaUserCircle className="w-20 h-20 text-gray-400" />
-            )}
-            <div className="flex flex-col">
-              <h2 className="font-bold text-lg">
-                {hoveredUser.name || "Unnamed User"}
-              </h2>
-              <div className="flex gap-1">
-                <IoLocationOutline />
-                <p className="text-black text-sm">
-                  {hoveredUser.city ? hoveredUser.city : "No city data"}
-                </p>
-              </div>
-              <p className="text-sm">
-                {hoveredUser.bio || "No bio available."}
-              </p>
-              <p className="text-sm">
-                {hoveredUser.website ? (
-                  <a
-                    href={hoveredUser.website}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-500"
-                  >
-                    {hoveredUser.website}
-                  </a>
-                ) : (
-                  "No website available."
-                )}
-              </p>
-            </div>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
